4adbbc6f0b7b6afb55d520d285a5ebeb47dc2123
Juan Gomez <j.gomez@drexed.com>
2019-07-09 16:49:10 -0400

    Rework config spec

--- spec/lite/uxid/configuration_spec.rb
+++ spec/lite/uxid/configuration_spec.rb
@@ -6,28 +6,14 @@ RSpec.describe Lite::Uxid::Configuration do
   after do
     Lite::Uxid.configure do |config|
       config.encoding_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-      config.encoding_length = 26
-      config.encoding_salt = 1_369_136
     end
   end
 
   describe '#configure' do
-    it 'to be "foo" for encoding_chars' do
+    it 'to be "foo"' do
       Lite::Uxid.configuration.encoding_chars = 'foo'
 
       expect(Lite::Uxid.configuration.encoding_chars).to eq('foo')
-    end
-
-    it 'to be "foo" for encoding_length' do
-      Lite::Uxid.configuration.encoding_length = 'foo'
-
-      expect(Lite::Uxid.configuration.encoding_length).to eq('foo')
-    end
-
-    it 'to be "foo" for encoding_salt' do
-      Lite::Uxid.configuration.encoding_salt = 'foo'
-
-      expect(Lite::Uxid.configuration.encoding_salt).to eq('foo')
     end
   end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,3 +28,9 @@ RSpec.configure do |config|
     FileUtils.remove_dir(temp_path) if File.directory?(temp_path)
   end
 end
+
+Lite::Uxid.configure do |config|
+  config.encoding_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  config.encoding_length = 26
+  config.encoding_salt = 1_369_136
+end
